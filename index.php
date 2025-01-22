2611f7504e3d84a447e96f99b1338a9fef77b74e
ipalv9 <ipal7271@gmail.com>
2025-01-22 10:47:45 +0700

    meanmbah kan memperbaiki beberapa aset

--- index.php
+++ index.php
@@ -1,33 +1,36 @@
+<?php
+include_once("ceklogin.php");
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Projek New Ipal</title>
+    <title>Project IS62</title>
     <link rel="stylesheet" href="css/bootstrap.css">
-    <link rel="stylesheet" href="css/all.css">
 </head>
 <body>
-<?php INCLUDE('navbar.php');
+<?php 
+    include_once('navbar.php');
 ?>
+
 <div class="container">
     <div class="row mt-5">
         <div class="col-6 m-auto">
-        <div class="card">
+            <div class="card">
             <div class="card-header text-center">
-                <h3>Sistem Informasi Mahasiswa</h3>
+                <h3>Sistem Informasi Produk</h3>
             </div>
-            <div class="card-body text-center">
-                <h4>Selamat Datang Di Kampung Halaman</h4>
-                <p>Tetap Menyerah</p>
+            <div class="card-body">
+                <h4>Selamat Datang, Administrator</h4>
+                <p>Tuliskan kata-kata hari ini</p>
             </div>
-        </div>
+            </div>
         </div>
     </div>
 </div>
 
-<script src="js/bootstrap.js"></script>
-<script src="js/bootstrap.bundle.js"></script>
-<script src="js/all.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/bootstrap.bundle.js"></script>
 </body>
 </html>
--- navbar.php
+++ navbar.php
@@ -7,16 +7,16 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/ipa/\uas_asfalasafia/index.php">Beranda</a>
+          <a class="nav-link active" aria-current="page" href="/ipa/uas_asfalasafia/index.php">Beranda</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Data Mahasiswa</a>
+          <a class="nav-link active" aria-current="page" href="/ipa/uas_asfalasafia/index.php">Data Produk</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/ipal/uas_asfalasafia/jurusan/index.php">Data Jurusan</a>
+          <a class="nav-link active" aria-current="page" href="/ipal/uas_asfalasafia/jurusan/index.php">Data kategori</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/\ipal/uas_asfalasafia/dosen/index.php">Data Dosen</a>
+          <a class="nav-link active" aria-current="page" href="/ipal/uas_asfalasafia/dosen/index.php">Data Dosen</a>
         </li>
           <ul class="dropdown-menu">
             <li><a class="dropdown-item" href="#">Action</a></li>
